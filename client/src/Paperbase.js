07428952490ae4a2446f65f130967686132d0458
Jason Godmere <jgodmere808@gmail.com>
2022-02-21 16:37:50 -1000

    Functional mostly-static component structure completed for Navigator, window, content and header (mui template implemented)

--- client/src/Paperbase.js
+++ client/src/Paperbase.js
@@ -201,7 +201,15 @@ export default function Paperbase() {
           />
         </Box>
 
-        <PageContent />
+        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+          <Header onDrawerToggle={handleDrawerToggle} />
+          <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
+            <Content />
+          </Box>
+          <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
+            <Copyright />
+          </Box>
+        </Box>
 
       </Box>
     </ThemeProvider>
--- client/src/components/window/Window.js
+++ client/src/components/window/Window.js
@@ -1,16 +1,19 @@
 import * as React from "react";
+import Box from '@mui/material/Box';
 
 import Header from "./header/Header.js";
 import Content from "./content/Content.js";
 
 
-function Window() {
+export default function Window() {
     return (
-        <React.Fragment>
-            <Header />
+    <React.Fragment>
+        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+          <Header />
+          <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
             <Content />
-        </React.Fragment>
+          </Box>
+        </Box>
+    </React.Fragment>
     );
 }
-
-export default Window;
